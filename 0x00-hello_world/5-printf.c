7199e1e600de419660c10858eeb6d3274a0868e8
osamankapit <muhammadmujahidtahir@gmail.com>
2023-02-16 13:12:20 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Description: Prints "with proper gr* ammar, but the outcome is a peice * of art,"
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but theoutcome is a piece of art,\n");
+return (0);
+}
