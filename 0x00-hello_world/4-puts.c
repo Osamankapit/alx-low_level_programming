a366ab14849fbb33ee2bcf1c88a7db820707e84c
osamankapit <muhammadmujahidtahir@gmail.com>
2023-02-16 10:47:03 -0800

    printf_sub

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -entry point
+*
+* Description: prints "Programming is like building a multilingual puzzle"
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"progrmamming is like building a multilingual puzzle");
+return (0);
+}
