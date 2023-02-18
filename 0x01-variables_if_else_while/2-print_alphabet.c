36e8a1f23ab2b6d0c82c2e0e5449767cc7f525dd
osamankapit <muhammadmujahidtahir@gmail.com>
2023-02-18 02:24:19 -0800

    printabc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+/**
+* main - prints the letters of the alphabet in lowercases
+* Description: for main, variable a-z
+*putchar to print out all alphabet in*loop and the second printing a new line
+*return  always 0 (succes)s
+*/
+
+{
+char letter = a
+while (letter <= z)
+{
+putchar(letter);
+letter++;
+}
+putchar(\n);
+return (0);
+}
+
